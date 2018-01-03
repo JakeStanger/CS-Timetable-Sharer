3f8f7a272fbcc95dd4a367193860f57e187c1250
Jake Stanger <jakestanger@gmail.com>
2018-01-03 15:55:46 +0000

    Implemented login/signup system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  # Used for getting the current user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # Used to prohibit access to logged in users
+  def require_user
+    redirect_to '/login' unless current_user
+  end
 end

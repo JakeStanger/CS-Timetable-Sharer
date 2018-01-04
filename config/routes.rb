2831befdd39ebc6a8833ec8e7f2318207d9fba19
Jake Stanger <jakestanger@gmail.com>
2018-01-04 20:05:53 +0000

    Added subjects

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Root page
   root 'users#show'
 
+  # Login and signup
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resource :users
 
+  # User accounts
   get 'users/:id' => 'users#show', as: :user
+
+  # Subjects
+  get 'subjects' => 'subjects#index'
+  get 'subjects/:id' => 'subjects#show', as: :subject
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103192933) do
+ActiveRecord::Schema.define(version: 20180104194914) do
 
   create_table "associates", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180103192933) do
   end
 
   create_table "friends", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
